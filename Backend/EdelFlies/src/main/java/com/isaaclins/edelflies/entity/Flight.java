58d7e16ec153a7e979811d792113c3cfc2d49cc1
Isaac Lins <isaac.lins07@gmail.com>
2024-06-21 11:03:45 +0200

    chore: Move Flight.java to correct directory

--- Backend/EdelFlies/src/main/java/com/isaaclins/edelflies/entity/Flight.java
+++ Backend/EdelFlies/src/main/java/com/isaaclins/edelflies/entity/Flight.java
@@ -1,22 +1,29 @@
 package com.isaaclins.edelflies.entity;
 
-import com.fasterxml.jackson.annotation.JsonBackReference;
-import lombok.Data;
-
+import lombok.*;
 import jakarta.persistence.*;
+
 import java.io.Serializable;
 import java.time.LocalDateTime;
 
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
 @Data
 @Entity
-public class Flight implements Serializable {
+public class Flight {
 
 	@Id
+	@NonNull
 	private int id;
+
+	@NonNull
 	private Long number;
 	private String cityFrom;
 	private String cityTo;
 	private LocalDateTime flightDate;
+	private int PlaneIDFS;
 
 
 
