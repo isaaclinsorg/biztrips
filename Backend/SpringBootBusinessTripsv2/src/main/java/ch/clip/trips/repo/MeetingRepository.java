e18c094fa99a1e7db062449568fb75c650706384
Isaac Lins <isaac.lins07@gmail.com>
2024-06-21 12:14:33 +0200

    chore: Remove unnecessary database configuration files and unused code

--- Backend/SpringBootBusinessTripsv2/src/main/java/ch/clip/trips/repo/MeetingRepository.java
+++
@@ -1,9 +0,0 @@
-package ch.clip.trips.repo;
-
-import org.springframework.data.repository.CrudRepository;
-
-import java.util.List;
-
-public interface MeetingRepository extends CrudRepository<Meeting, Long> {
-	List<Meeting> findByTitle(String title);
-}
