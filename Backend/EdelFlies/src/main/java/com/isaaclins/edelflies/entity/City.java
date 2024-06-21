4079d9fcdc50727f1473e6e7d843ef5ce8912671
Isaac Lins <isaac.lins07@gmail.com>
2024-06-21 15:51:00 +0200

    feat: Add City entity class

---
+++ Backend/EdelFlies/src/main/java/com/isaaclins/edelflies/entity/City.java
@@ -0,0 +1,25 @@
+package com.isaaclins.edelflies.entity;
+
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import lombok.*;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+@Data
+@Entity
+public class City {
+    @Id
+    private int id;
+
+    @NonNull
+
+    private String fullName;
+
+    @NonNull
+    private String abbreviation;
+
+}
--- Backend/EdelFlies/src/main/java/com/isaaclins/edelflies/entity/Location.java
+++ Backend/EdelFlies/src/main/java/com/isaaclins/edelflies/entity/Location.java
@@ -1,24 +1,3 @@
-package com.isaaclins.edelflies.entity;
-
-
-import jakarta.persistence.Entity;
-import jakarta.persistence.Id;
-import lombok.*;
-
-@AllArgsConstructor
-@NoArgsConstructor
-@Getter
-@Setter
-@Data
-@Entity
 public class Location {
-    @Id
-    private int id;
-
-    @NonNull
-    private String fullName;
-
-    @NonNull
-    private String abbreviation;
-
+    
 }
