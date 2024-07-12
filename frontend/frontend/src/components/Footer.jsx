828b9f7e87df0807ef001b6122c6c82efcb6840f
3RISJAK <114018548+eri6081@users.noreply.github.com>
2024-07-12 22:49:24 +0200

    improved view

--- frontend/frontend/src/components/Footer.jsx
+++ frontend/frontend/src/components/Footer.jsx
@@ -3,7 +3,7 @@ import React from "react";
 export default function Footer() {
   return (
     <footer>
-      <p>&copy; BBW M426 Isaac Lins, Eris Jakupi, Patrick Pilotti 2024</p>
+      <p>&copy; BBW M426 Isaac Lins & Eris Jakupi & Patrick Pilotti 2024</p>
     </footer>
   );
 }
--- frontend/frontend/src/components/Header.jsx
+++ frontend/frontend/src/components/Header.jsx
@@ -10,29 +10,27 @@ export default function Header() {
   return (
       <header className="header-container">
         <nav>
-          <ul>
+          <ul className="navbar">
             <li>
               <Link to="/">
-                <img alt="BBW Fitness" src="/images/logo.png"/>
+                <img className="logo" alt="BBW Fitness" src="/images/logo.png" />
               </Link>
             </li>
-            <ul className="navbar">
-              <li>
-                <NavLink activeStyle={activeStyle} to="/trips">
-                  E-Tickets
-                </NavLink>
-              </li>
-              <li>
-                <NavLink activeStyle={activeStyle} to="/wishlist">
-                  Warenkorb
-                </NavLink>
-              </li>
-              <li id="login">
-                <NavLink activeStyle={activeStyle} to="/login">
-                  Login
-                </NavLink>
-              </li>
-            </ul>
+            <li>
+              <NavLink activeStyle={activeStyle} to="/trips">
+                E-Tickets
+              </NavLink>
+            </li>
+            <li>
+              <NavLink activeStyle={activeStyle} to="/wishlist">
+                Warenkorb
+              </NavLink>
+            </li>
+            <li id="login">
+              <NavLink activeStyle={activeStyle} to="/login">
+                Login
+              </NavLink>
+            </li>
           </ul>
         </nav>
       </header>
