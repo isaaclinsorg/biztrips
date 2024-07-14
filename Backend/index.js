da49e0d09fa4365e4f1708e9a353bd599848612c
Isaac Lins <isaac.lins07@gmail.com>
2024-07-14 12:14:25 +0200

    chore: Update server port to 3001

--- Backend/index.js
+++ Backend/index.js
@@ -1100,6 +1100,6 @@ app.delete('/userflights/:id', (req, res) => {
     }
 });
 
-app.listen(3000, () => {
-    console.log('Server is running on port 3000');
+app.listen(3001, () => {
+    console.log('Server is running on port 3001');
 });
